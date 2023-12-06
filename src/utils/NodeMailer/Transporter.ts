d89940541caf89d4374589b3268265e7e688d2c5
Robson Nonato <robsonnonatoiii@gmail.com>
2023-12-06 17:51:57 -0300

    Verificar Email 3

--- src/utils/NodeMailer/Transporter.ts
+++ src/utils/NodeMailer/Transporter.ts
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_LOGIN,
     pass: process.env.GMAIL_PSWD,
