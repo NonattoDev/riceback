3377174c689db3bce445ac999fe3ee5293974c3d
Robson Nonato <108809490+NonattoDev@users.noreply.github.com>
2023-12-15 08:39:24 -0300

    Sobre

--- src/pages/sobre/index.tsx
+++ src/pages/sobre/index.tsx
@@ -9,9 +9,13 @@ const Sobre: React.FC = () => {
         <h1 className="text-4xl font-bold my-4">Sobre a Riceback</h1>
       </div>
       <p className="text-lg leading-relaxed">
-        A <span className="font-semibold">Riceback</span> é uma organização não governamental que tem como objetivo gerar renda através da redução do desperdício de arroz. Nossa missão é evitar o
-        descarte desnecessário desse alimento essencial, transformando-o em oportunidades para aqueles que mais precisam.
+        A <span className="font-semibold">Riceback</span> é um aplicativo que propõe vender uma porção social de arroz em restaurantes participantes. O valor arrecadado com a venda dessa porção social
+        é revertido em dinheiro para os restaurantes.
       </p>
+      <p className="text-lg leading-relaxed">
+        Essa abordagem visa combater o desperdício de alimentos, especificamente o desperdício de arroz, ao mesmo tempo em que apoia os restaurantes e gera impacto social.
+      </p>
+      <p className="text-lg leading-relaxed">É uma iniciativa interessante que busca criar um modelo de negócio sustentável e ao mesmo tempo contribuir para a comunidade.</p>
       {/* Adicione mais conteúdo conforme necessário */}
     </div>
   );
